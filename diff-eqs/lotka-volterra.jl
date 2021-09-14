bd4592d5af279ecc44c8deb42a393f7e925fcbd7
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-09-14 14:31:51 +1000

    Initial DE code

--- diff-eqs/lotka-volterra.jl
+++ diff-eqs/lotka-volterra.jl
@@ -9,4 +9,22 @@
 
 using Plots, DifferentialEquations
 
+params = (2.0, .5, .2, .6)
 
+function myLV(Δu, u, params, t)
+
+    α, β, γ, δ = params
+
+    Δu[1] = α * u[1] -β * u[1] * u[2]
+    Δu[2] = -δ * u[2]+ γ * u[1] * u[2]
+end
+
+u0 = [10.0; 10.0]
+tspan = (0.0, 100.0)
+
+prob = ODEProblem(myLV, u0, tspan, params)
+sol = solve(prob)
+
+# Plot
+
+plot(sol, title = "Lotka-Volterra system", xlabel = "Time", ylabel = "Number")
